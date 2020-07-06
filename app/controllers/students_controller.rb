883e0ff475cb853cd6562c3ad50241b90964e5eb
Adamlatorraca <adam.latorraca@gmail.com>
2020-07-06 18:55:30 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -6,6 +6,19 @@ class StudentsController < ApplicationController
   end
 
   def show
+    set_student
+  end
+
+  def activate
+    set_student
+    if @student.active
+      @student.active = false
+    else
+      @student.active = true
+    end
+    @student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
