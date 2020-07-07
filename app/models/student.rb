4697a1eab8dc299ebd474a7de6f55ab95437e026
mkopsho <michael.kopsho@gmail.com>
2020-07-07 13:46:00 -0400

    Done.

--- app/models/student.rb
+++ app/models/student.rb
@@ -2,4 +2,12 @@ class Student < ActiveRecord::Base
   def to_s
     self.first_name + " " + self.last_name
   end
+
+  def active_status
+    if self.active == false
+      active_status = "inactive"
+    else
+      active_status = "active"
+    end
+  end
 end
