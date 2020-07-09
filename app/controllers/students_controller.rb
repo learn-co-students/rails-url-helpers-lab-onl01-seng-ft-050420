f716d88a4068ad34697b28120669a7e26b809f0a
codewithirene567 <irenejinscott@gmail.com>
2020-07-09 13:28:08 -0400

    Done.

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -9,6 +9,17 @@ class StudentsController < ApplicationController
     @student = Student.find(params[:id])
   end
 
+    def activate
+      @student = Student.find(params[:id])
+      @student.active = !@student.active
+      @student.save
+      redirect_to student_path
+    end
+      #find the student, if the status is true then make it false and vice versa
+      #then save the change and redirect to show page
+    #define what active means
+  
+
   private
 
     def set_student
